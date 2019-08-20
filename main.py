cb1dcf239e507b86e7d5d57c55b146836fb62f77
Will Klassen <william_klassen@hotmail.com>
2019-08-19 21:56:25 -0400

    training fix

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ import cv2
 
 from keras.models import Sequential
 from keras.layers.convolutional import Conv2D, MaxPooling2D
-from keras.layers import Flatten, Dense
+from keras.layers import Flatten, Dense, Activation, Dropout
 
 # from keras.layers import Input, Concatenate, GlobalAvgPool2D, GlobalMaxPool2D, Subtract, Multiply, Dense, Flatten, Dropout
 # from keras.models import Model, model_from_json
@@ -20,7 +20,7 @@ from keras.callbacks import ModelCheckpoint
 # global variables
 learning_rate       = 0.00001
 epochs              = 100
-batch_size          = 5
+batch_size          = 7
 steps_per_epoch     = 200
 validation_steps    = 200
 input_shape         = (224, 224, 3)
@@ -30,13 +30,34 @@ num_classes         = 5
 
 # create and save the model structure
 def create_model(model_name):
+    # model = Sequential()
+    # model.add(Conv2D(32, kernel_size=(5, 5), strides=(1, 1), activation='relu', input_shape=input_shape))
+    # model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
+    # model.add(Conv2D(64, (5, 5), activation='relu'))
+    # model.add(MaxPooling2D(pool_size=(2, 2)))
+    # model.add(Flatten())
+    # model.add(Dense(1000, activation='relu'))
+    # model.add(Dense(num_classes, activation='sigmoid'))
+
+
     model = Sequential()
-    model.add(Conv2D(32, kernel_size=(5, 5), strides=(1, 1), activation='relu', input_shape=input_shape))
-    model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
-    model.add(Conv2D(64, (5, 5), activation='relu'))
+    model.add(Conv2D(32, (3, 3), input_shape=(224, 224, 3)))
+    model.add(Activation('relu'))
     model.add(MaxPooling2D(pool_size=(2, 2)))
-    model.add(Flatten())
-    model.add(Dense(1000, activation='relu'))
+
+    model.add(Conv2D(32, (3, 3)))
+    model.add(Activation('relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+
+    model.add(Conv2D(64, (3, 3)))
+    model.add(Activation('relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+
+    model.add(Flatten())  # this converts our 3D feature maps to 1D feature vectors
+    model.add(Dense(64))
+    model.add(Activation('relu'))
+    model.add(Dropout(0.5))
+
     model.add(Dense(num_classes, activation='sigmoid'))
 
     # save model to json file
@@ -63,8 +84,10 @@ def read_image(image_path):
 
 # generator for producing batches of images realtion arrays to train on
 def my_gen(training_set, image_names_to_paths, batch_size=16):
-    persons = list(image_names_to_paths.keys())
-    num_persons = len(persons)
+    # persons = list(image_names_to_paths.keys())
+    # num_persons = len(persons)
+    # print(num_persons)
+    # input()
     while True:
         training_batch = random.sample(training_set, batch_size)
         labels = []
@@ -82,20 +105,19 @@ def my_gen(training_set, image_names_to_paths, batch_size=16):
         # batch_images = np.array([read_image(image_path) for image_path in batch_image_paths])
         batch_images = []
         for image_path in batch_image_paths:
-            print('__', image_path)
+            # print('__', image_path)
             temp = read_image(image_path)
-            print('__', temp.shape)
+            # print('__', temp.shape)
             batch_images.append(temp)
-        batch_images = np.array(batch_images)
-        print(batch_images.shape)
+        # batch_images = np.array(batch_images)
+
+        print(np.array([batch_images]).shape)
+        print(np.array([labels]).shape)
         input()
 
-        # print('!!')
-        # print(batch_images.shape)
-        training_batch_final = ([batch_images], labels)
-        # input()
+        # training_batch_final = ([batch_images], labels)
 
-        yield training_batch_final
+        yield ([np.array(batch_images)], [np.array(labels)])
 
     return
 
